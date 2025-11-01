85008cfbef6ee09aafcac252b01b117e174db1a3
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2025-11-01 16:46:21 -0400

    Update synthesizer_vst.cpp

--- src/synthesizer_vst.cpp
+++ src/synthesizer_vst.cpp
@@ -60,11 +60,11 @@ static VstIntPtr VSTCALLBACK HostCallback(AEffect *effect, VstInt32 opcode, VstI
      break;
 
    case audioMasterProcessEvents:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterIOChanged:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case DECLARE_VST_DEPRECATED(audioMasterNeedIdle):
@@ -72,7 +72,7 @@ static VstIntPtr VSTCALLBACK HostCallback(AEffect *effect, VstInt32 opcode, VstI
      break;
 
    case audioMasterSizeWindow:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterGetSampleRate:
@@ -92,11 +92,11 @@ static VstIntPtr VSTCALLBACK HostCallback(AEffect *effect, VstInt32 opcode, VstI
      break;
 
    case audioMasterGetInputLatency:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterGetOutputLatency:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
 
@@ -109,27 +109,27 @@ static VstIntPtr VSTCALLBACK HostCallback(AEffect *effect, VstInt32 opcode, VstI
      break;
 
    case audioMasterGetAutomationState:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterOfflineStart:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterOfflineRead:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterOfflineWrite:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterOfflineGetCurrentPass:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterOfflineGetCurrentMetaPass:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterGetVendorString:
@@ -151,43 +151,43 @@ static VstIntPtr VSTCALLBACK HostCallback(AEffect *effect, VstInt32 opcode, VstI
      break;
 
    case audioMasterVendorSpecific:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterCanDo:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterGetLanguage:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterGetDirectory:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterUpdateDisplay:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterBeginEdit:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterEndEdit:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterOpenFileSelector:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    case audioMasterCloseFileSelector:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
 
    default:
-     printf("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
+     printf("PLUG> HostCallback (Opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void>(value), ptr, opt);
      break;
   }
 
@@ -214,7 +214,7 @@ void check_effect_properties(AEffect *effect) {
   for (VstInt32 progIndex = 0; progIndex < effect->numPrograms; progIndex++) {
     char progName[256] = {0};
     if (!effect->dispatcher(effect, effGetProgramNameIndexed, progIndex, 0, progName, 0)) {
-      effect->dispatcher(effect, effSetProgram, 0, progIndex, 0, 0);        // Note: old program not restored here!
+      effect->dispatcher(effect, effSetProgram, 0, progIndex, 0, 0);        // Note: Old program not restored here!
       effect->dispatcher(effect, effGetProgramName, 0, 0, progName, 0);
     }
     printf("Program %03d: %s\n", progIndex, progName);
@@ -249,9 +249,9 @@ void check_effect_properties(AEffect *effect) {
     printf("Can do %s... ", canDos[canDoIndex]);
     VstInt32 result = (VstInt32)effect->dispatcher(effect, effCanDo, 0, 0, (void *)canDos[canDoIndex], 0);
     switch (result) {
-     case 0: printf("don't know"); break;
-     case 1: printf("yes"); break;
-     case -1: printf("definitely not!"); break;
+     case 0: printf("Don't know"); break;
+     case 1: printf("Yes"); break;
+     case -1: printf("Definitely not!"); break;
      default: printf("?????");
     }
     printf("\n");
@@ -260,22 +260,22 @@ void check_effect_properties(AEffect *effect) {
   printf("\n");
 }
 
-// load plugin
+// Load plugin
 int vsti_load_plugin(const char *path) {
   thread_lock lock(vsti_thread_lock);
 
-  // unload plugin first
+  // Unload plugin first
   vsti_unload_plugin();
 
   typedef AEffect * (*PluginEntryProc)(audioMasterCallback audioMaster);
 
-  // load library
+  // Load library
   module = LoadLibrary(path);
 
   if (module == NULL)
     return -1;
 
-  // get effect constructor.
+  // Get effect constructor.
   PluginEntryProc mainProc = 0;
   mainProc = (PluginEntryProc)GetProcAddress((HMODULE)module, "VSTPluginMain");
 
@@ -285,7 +285,7 @@ int vsti_load_plugin(const char *path) {
   if (!mainProc)
     return -1;
 
-  // create effect instance
+  // Create effect instance
   effect = mainProc(HostCallback);
 
   if (effect && (effect->magic != kEffectMagic))
@@ -294,43 +294,43 @@ int vsti_load_plugin(const char *path) {
   if (effect == NULL)
     return NULL;
 
-  // open effect
+  // Open effect
   effect->dispatcher(effect, effOpen, 0, NULL, 0, 0);
   effect->dispatcher(effect, effBeginSetProgram, 0, NULL, 0, 0);
   effect->dispatcher(effect, effSetProgram, 0, 0, 0, 0);
   effect->dispatcher(effect, effEndSetProgram, 0, NULL, 0, 0);
 
-  // reset effect flags
+  // Reset effect flags
   effect_processing = 0;
 
 #ifdef _DEBUG
-  // check effect properties
-  //check_effect_properties(effect);
+  // Check effect properties
+  // check_effect_properties(effect);
 #endif
 
-  // show editor
+  // Show editor
   vsti_show_editor(effect_show_editor);
 
   return 0;
 }
 
-// unload plugin
+// Unload plugin
 void vsti_unload_plugin() {
   thread_lock lock(vsti_thread_lock);
 
-  // destroy effect window
+  // Destroy effect window
   if (editor_window) {
     DestroyWindow(editor_window);
     editor_window = NULL;
   }
 
-  // close effect
+  // Close effect
   if (effect) {
     effect->dispatcher(effect, effClose, 0, NULL, 0, 0);
     effect = NULL;
   }
 
-  // unload effect dll
+  // Unload effect DLL
   if (module) {
     FreeLibrary(module);
     module = NULL;
@@ -343,7 +343,7 @@ void vsti_unload_plugin() {
 }
 
 
-// send midi event to
+// Send MIDI event to
 void vsti_send_midi_event(byte data1, byte data2, byte data3, byte data4) {
   thread_lock lock(vsti_thread_lock);
 
@@ -368,7 +368,7 @@ void vsti_send_midi_event(byte data1, byte data2, byte data3, byte data4) {
   }
 }
 
-// stop output
+// Stop output
 void vsti_stop_process() {
   thread_lock lock(vsti_thread_lock);
 
@@ -400,12 +400,12 @@ void vsti_update_config(float samplerate, uint blocksize) {
   }
 }
 
-// process
+// Process
 void vsti_process(float *left, float *right, uint buffer_size) {
   thread_lock lock(vsti_thread_lock);
 
   if (effect && effect_processing) {
-    // process events
+    // Process events
     struct MoreEvents : VstEvents {
       VstEvent *data[256];
     }
@@ -417,17 +417,17 @@ void vsti_process(float *left, float *right, uint buffer_size) {
     for (uint i = 0; i < midi_event_count; i++)
       buffer.events[i] = (VstEvent *)&midi_event_buffer[i];
 
-    // process events
+    // Process events
     effect->dispatcher(effect, effProcessEvents, 0, 0, &buffer, 0);
 
-    // clear processed events
+    // Clear processed events
     midi_event_count = 0;
 
-    // clear data
+    // Clear data
     memset(left, 0, buffer_size * sizeof(float));
     memset(right, 0, buffer_size * sizeof(float));
 
-    // TODO: vsti has it's own buffer count
+    // TODO: VSTi has its own buffer count
     float *outputs[64] = { left, right };
     float *inputs[64] = {0};
 
@@ -449,9 +449,9 @@ void vsti_process(float *left, float *right, uint buffer_size) {
 }
 
 // -----------------------------------------------------------------------------------------
-// vsti editor functions
+// VSTi editor functions
 // -----------------------------------------------------------------------------------------
-// vst window proc
+// VST window proc
 static LRESULT CALLBACK vst_editor_wndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   switch (uMsg) {
    case WM_CREATE:
@@ -490,12 +490,12 @@ static LRESULT CALLBACK vst_editor_wndproc(HWND hWnd, UINT uMsg, WPARAM wParam, 
 }
 
 
-// create main window
+// Create main window
 static HWND create_effect_window(AEffect *effect) {
   static bool initialized = false;
 
   if (!initialized) {
-    // register window class
+    // Register window class
     WNDCLASSEX wc = { sizeof(wc), 0 };
     wc.style = CS_DBLCLKS;
     wc.lpfnWndProc = &vst_editor_wndproc;
@@ -517,7 +517,7 @@ static HWND create_effect_window(AEffect *effect) {
   char effectName[256] = {0};
   effect->dispatcher(effect, effGetEffectName, 0, 0, effectName, 0);
 
-  // create window
+  // Create window
   HWND hwnd = CreateWindow("FreePianoVstEffect", effectName, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, NULL, NULL, GetModuleHandle(NULL), NULL);
 
   if (hwnd) {
@@ -548,33 +548,33 @@ static HWND create_effect_window(AEffect *effect) {
   return hwnd;
 }
 
-// is show eidtor
+// Is show eidtor
 bool vsti_is_show_editor() {
   return effect_show_editor;
 }
 
-// show effect editor
+// Show effect editor
 void vsti_show_editor(bool show) {
   thread_lock lock(vsti_thread_lock);
 
-  // set flag
+  // Set flag
   effect_show_editor = show;
 
-  // no effect loaded
+  // No effect loaded
   if (!effect)
     return;
 
-  // effect has no editor
+  // Effect has no editor
   if ((effect->flags & effFlagsHasEditor) == 0)
     return;
 
   if (show) {
-    // create effect window
+    // Create effect window
     if (!editor_window) {
       editor_window = create_effect_window(effect);
     }
   } else   {
-    // destroy effect window
+    // Destroy effect window
     if (editor_window) {
       DestroyWindow(editor_window);
       editor_window = NULL;
@@ -600,7 +600,7 @@ static void search_plugins(const char *path, vsti_enum_callback &callback) {
 
   FindClose(finddata);
 
-  // serch subdirectoies
+  // Search subdirectoies
   _snprintf(buffer, sizeof(buffer), "%s\\*", path);
   finddata = FindFirstFileA(buffer, &data);
 
@@ -620,7 +620,7 @@ static void search_plugins(const char *path, vsti_enum_callback &callback) {
   FindClose(finddata);
 }
 
-// vst enum plugins
+// VST enum plugins
 void vsti_enum_plugins(vsti_enum_callback &callback) {
   HKEY key = HKEY_LOCAL_MACHINE;
 
@@ -640,8 +640,9 @@ void vsti_enum_plugins(vsti_enum_callback &callback) {
 }
 
 
-// is instrument loaded
+// Is instrument loaded
 bool vsti_is_instrument_loaded() {
   thread_lock lock(vsti_thread_lock);
   return effect != NULL;
+
 }
