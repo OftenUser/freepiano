c6ef8539c7b1b6495dedea263215071bfd947701
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2025-11-01 16:36:51 -0400

    Update language.h

--- src/language.h
+++ src/language.h
@@ -5,7 +5,7 @@
 #define FP_LANG_SCHINESE    1
 #define FP_LANG_COUNT       2
 
-// language string def
+// Language string def
 #define STR_ENGLISH(id, str)  id,
 #define STR_SCHINESE(id, str) 
 enum StringIDs {
@@ -16,41 +16,42 @@ enum StringIDs {
 #undef STR_ENGLISH
 #undef STR_SCHINESE
 
-// initialize languages
+// Initialize languages
 void lang_init();
 
-// select current language
+// Select current language
 void lang_set_current(int languageid);
 
-// get current language
+// Get current language
 int lang_get_current();
 
-// load str
+// Load str
 const char* lang_load_string(uint uid);
 
-// load lang string array
+// Load lang string array
 const char** lang_load_string_array(uint uid);
 
-// format lang str
+// Format lang str
 int lang_format_string(char *buff, size_t size, uint strid, ...);
 
-// open text
+// Open text
 int lang_text_open(uint textid);
 
-// readline
+// Readline
 int lang_text_readline(char *buff, size_t size);
 
-// close text
+// Close text
 void lang_text_close();
 
-// set last error
+// Set last error
 void lang_set_last_error(const char *format, ...);
 
-// set last error
+// Set last error
 void lang_set_last_error(uint id, ...);
 
-// get last error
+// Get last error
 const char * lang_get_last_error();
 
-// localize dialog
+// Localize dialog
+
 void lang_localize_dialog(HWND hwnd);
